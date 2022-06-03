5716063b3fb97c60a5e7bae76dd0986c4adea619
sahanaka <weraniyagoda97@gmail.com>
2022-06-03 22:31:15 +0530

    fixed datasource issue

--- src/main/java/com/example/studentlms/StudentlmsApplication.java
+++ src/main/java/com/example/studentlms/StudentlmsApplication.java
@@ -2,8 +2,9 @@ package com.example.studentlms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class StudentlmsApplication {
 
 	public static void main(String[] args) {
